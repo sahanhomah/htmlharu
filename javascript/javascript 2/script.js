847075df049855a6fefecb1439d7faca96239b6e
Sahan <sahanshrestha2000@gmail.com>
2025-02-24 21:04:24 +0545

    javascriptc2

--- javascript/javascript 2/script.js
+++ javascript/javascript 2/script.js
@@ -1,7 +1,33 @@
-let a=5;
-let b=6;
+// let a=5;
+// let b=6;
 
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// let condition1 = a==3;
+// let condition2 = b>=3;
+// console.log(condition1||condition2);
+// let age=42;
+// if(age<=18){
+//     console.log("baccha");
+// }
+// else if(age<=40){
+//     console.log("bau");
+
+// }
+// else{
+//     console.log("hajurbau");
+// }
+// let time='night';
+// time=='day'?console.log("nabaal"):console.log("baal")
+// alert("hello");
+let a=prompt("enter");
+if(a%2==0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+
+
