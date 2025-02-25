ee340d6c6713530ac1d3b9c3c48dae0f5ebeb957
Sahan <sahanshrestha2000@gmail.com>
2025-02-25 23:07:47 +0545

    javascript video 3

---
+++ javascript/javascript 3/script.js
@@ -0,0 +1,14 @@
+// let name="Sahanshrestha";
+// for(let i of name){
+//     console.log(i);
+// }
+// for(i=0;i<=100;i=i+2)
+// {
+//     console.log(i);
+// }
+let right=4;
+let user=prompt("guess the number");
+while(user!=4){
+    user=prompt("wrong so guess again");
+}
+console.log("you guessed it right");
