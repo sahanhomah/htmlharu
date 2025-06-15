983e179e4121f3a562b68a1670036719c930e32b
Sahan <sahanshrestha2000@gmail.com>
2025-06-15 21:57:07 +0545

    daraz

--- react/react1/test/src/App.js
+++ react/react1/test/src/App.js
@@ -1,16 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-var name="sshan";
-var element=<h1>big text</h1>
 
+const Daraz = () => {
+  return (
+    <div className="daraz-container" style={{}}>
+      <h1 className="title">Daraz</h1>
+      <input
+        className="search-input"
+        placeholder="Search Products"
+      />
+     <ol className="nav-list" type="A">
+  <li><a href="#home">Home</a></li>
+  <li><a href="#contact">Contact Us</a></li>
+  <li><a href="#report">Report</a></li>
+</ol>
+    </div>)
+}
 function App() {
   return (
     
     <div className="App">
-      {element}
-    <h1> Hello {name}</h1>
+     
+   
+   <Daraz/>
     </div>
-  );
+
+);
 }
 
 export default App;
