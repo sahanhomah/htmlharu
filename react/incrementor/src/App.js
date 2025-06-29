b8318e02fb378bf4cc8c87805306048ebcd95aa9
Sahan <sahanshrestha2000@gmail.com>
2025-06-29 16:01:42 +0545

    hooks1

--- react/incrementor/src/App.js
+++ react/incrementor/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 var count=0;
 function App() {
-
+const[value,setValue]=useState();
 
     const increaseNumber = () =>{
-      count=count+1;
-        console.log({count});
+    count=count+1;
+        setValue(count);
+      
     };
       const decreaseNumber = () =>{
-      count=count-1;
-        console.log({count});
+       count=count-1;
+        setValue(count);
     };
   return (
   
   <>
 <button class="btn" onClick={increaseNumber}>Increment</button>
 <button class="btn" onClick={decreaseNumber}>Decrement</button>
+<br></br>
 
+<>the value is {value}</>
 
 
 
